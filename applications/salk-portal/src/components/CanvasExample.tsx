e0b8ea07183dea2a870d75578d9c0e8be8244324
afonso <afonsobspinto@gmail.com>
2022-02-04 13:48:03 +0000

    Add atlas meshes example

--- applications/salk-portal/src/components/CanvasExample.tsx
+++ applications/salk-portal/src/components/CanvasExample.tsx
@@ -1,39 +1,66 @@
 // @ts-nocheck
 
 import React, {Component} from 'react';
+import * as THREE from 'three';
 import {withStyles} from '@material-ui/core';
 import Canvas from "@metacell/geppetto-meta-ui/3d-canvas/Canvas";
 import SimpleInstance from "@metacell/geppetto-meta-core/model/SimpleInstance";
 import {augmentInstancesArray} from '@metacell/geppetto-meta-core/Instances';
 import Resources from '@metacell/geppetto-meta-core/Resources';
-import obj from '../assets/neuron.obj';
+import ocord from '../assets/atlas_meshes/simplified/open_cord_simp.obj'
+import icord from '../assets/atlas_meshes/simplified/inside_cord_simp.obj'
 import DummyCameraControls from "./dummy/DummyCameraControls";
 import DummyCaptureControls from "./dummy/DummyCaptureControls";
+
+const COLOR_MAP = {
+    'OpenCordOBJ': {r: 0.25, g: 0.06, b: 0.25, a: 0.5},
+    'InsideCordOBJ': {r: 0.25, g: 0.06, b: 0.25, a: 0.5},
+}
+
+const YELLOW = 0xffff00
+const GREEN = 0x00FF00
+const BLUE = 0x0000FF
+
+const SPHERE_COLORS = [YELLOW, GREEN, BLUE]
+const SPHERES_COUNT = 1000
 
 
 function mapToCanvasData(data) {
     return data.map(item => (
         {
-            color: item.color,
-            instancePath: item.instancePath
+            instancePath: item.instancePath,
+            color: item.instancePath in COLOR_MAP ? COLOR_MAP[item.instancePath] : null
         }
     ))
 }
 
-const instance1spec = {
+
+const instance2spec = {
     "eClass": "SimpleInstance",
-    "id": "ExampleOBJ",
-    "name": "Example OBJ name",
+    "id": "OpenCordOBJ",
+    "name": "Open Cord OBJ",
     "type": {"eClass": "SimpleType"},
     "visualValue": {
         "eClass": Resources.OBJ,
-        'obj': obj
+        'obj': ocord
+    }
+}
+
+const instance3spec = {
+    "eClass": "SimpleInstance",
+    "id": "InsideCordOBJ",
+    "name": "Inside Cord OBJ",
+    "type": {"eClass": "SimpleType"},
+    "visualValue": {
+        "eClass": Resources.OBJ,
+        'obj': icord
     }
 }
 
 function loadInstances() {
-    const instance1 = new SimpleInstance(instance1spec)
-    window.Instances = [instance1]
+    const instance2 = new SimpleInstance(instance2spec)
+    const instance3 = new SimpleInstance(instance3spec)
+    window.Instances = [instance2, instance3]
     augmentInstancesArray(window.Instances);
 }
 
@@ -46,8 +73,8 @@ function getDefaultOptions() {
     return {
         'cameraOptions': {
             angle: 50,
-            near: 0.01,
-            far: 1000,
+            near: 0.000001,
+            far: 5000,
             baseZoom: 1,
             cameraControls: {
                 instance: DummyCameraControls,
@@ -110,6 +137,29 @@ class CanvasExample extends Component {
 
     // tslint:disable-next-line:no-empty
     onMount(scene) {
+        for (const color of SPHERE_COLORS) {
+            const geometry = new THREE.SphereGeometry(1, 32, 16);
+            const dummy = new THREE.Object3D();
+            const position = new THREE.Vector3();
+            const material = new THREE.MeshBasicMaterial({color});
+            const amount = 10
+            const mesh = new THREE.InstancedMesh(geometry, material, Math.pow(amount, 3));
+            mesh.frustumCulled = false
+            for (let i = 0; i < mesh.count; i++) {
+                position.set(
+                    Math.random() * 800,
+                    50 + Math.random() * 40,
+                    80 + Math.random() * 60
+                )
+
+                dummy.position.copy(position)
+                dummy.updateMatrix()
+
+                mesh.setMatrixAt(i, dummy.matrix);
+            }
+            scene.add(mesh);
+
+        }
     }
 
     // tslint:disable-next-line:no-empty
@@ -126,7 +176,7 @@ class CanvasExample extends Component {
                 data={canvasData}
                 cameraOptions={cameraOptions}
                 captureOptions={captureOptions}
-                backgroundColor={0x505050}
+                backgroundColor={0x000000}
                 onSelection={this.onSelection}
                 onMount={this.onMount}
                 hoverListeners={[this.hoverHandler]}
