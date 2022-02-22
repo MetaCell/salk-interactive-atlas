10f7d962be6d538894b02fc16fef533183604464
afonso <afonsobspinto@gmail.com>
2022-02-22 16:32:39 +0000

    Remove dummy folder

--- applications/salk-portal/src/components/dummy/DummyCameraControls.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class DummyCameraControls extends React.Component {
-    render() {
-        return <span/>;
-    }
-}
--- applications/salk-portal/src/components/dummy/DummyCaptureControls.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class DummyCaptureControls extends React.Component {
-    render() {
-        return <span/>;
-    }
-}
