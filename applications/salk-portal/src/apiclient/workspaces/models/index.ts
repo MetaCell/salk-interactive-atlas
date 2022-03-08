152752d8ea475770e94c2d82e8d800f10095c3d4
Zoran Sinnema <zsinnema@gmail.com>
2022-03-08 15:46:12 +0100

    #21 feature generated new frontend js api from openapi.yaml

--- applications/salk-portal/src/apiclient/workspaces/models/index.ts
+++ applications/salk-portal/src/apiclient/workspaces/models/index.ts
@@ -1,3 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './Experiment';
+export * from './ExperimentCollaborators';
+export * from './ExperimentTeams';
+export * from './ExperimentUser';
+export * from './Team';
+export * from './UserTeam';
