7a25a783c22b258ffd8ed30b6d6854d9c66c3086
afonso <afonsobspinto@gmail.com>
2022-03-11 15:16:06 +0000

    #32 Update serializers

--- applications/workspaces/backend/api/serializers.py
+++ applications/workspaces/backend/api/serializers.py
@@ -1,36 +1,59 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-from api.models import Experiment, Collaborator, CollaboratorRole
+from api.models import Experiment, Collaborator, CollaboratorRole, Population, AtlasesChoice
 from kcoidc.serializers import UserSerializer, GroupSerializer
+
 
 class CollaboratorRoleField(serializers.RelatedField):
     def to_representation(self, value):
         return {"role": value, "description": CollaboratorRole.to_str(value)}
 
+
 class CollaboratorSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     role = CollaboratorRoleField(read_only=True)
+
     class Meta:
         model = Collaborator
-        fields = ("user","role",)
-
-class ExperimentSerializer(serializers.ModelSerializer):
-    teams = GroupSerializer(many=True, read_only=True)
-    collaborators = CollaboratorSerializer(source='collaborator_set', many=True, read_only=True)
-    class Meta:
-        model = Experiment
-        fields = ("name","date_created","description","is_private","teams","collaborators")
+        fields = ("user", "role",)
 
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ("id","name")
+        fields = ("id", "name")
 
 
 class UserTeamSerializer(serializers.ModelSerializer):
     groups = TeamSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ("id","username","first_name","last_name","email","groups")
+        fields = ("id", "username", "first_name", "last_name", "email", "groups")
+
+
+class AtlasChoiceField(serializers.RelatedField):
+    def to_representation(self, value):
+        return {"role": value, "description": AtlasesChoice.to_str(value)}
+
+
+class PopulationSerializer(serializers.ModelSerializer):
+    atlas = AtlasChoiceField(read_only=True)
+
+    class Meta:
+        model = Population
+        fields = ("name", "color", "atlas")
+
+
+class ExperimentSerializer(serializers.ModelSerializer):
+    teams = GroupSerializer(many=True, read_only=True)
+    collaborators = CollaboratorSerializer(source='collaborator_set', many=True, read_only=True)
+    owner = UserTeamSerializer(many=False, read_only=True)
+    populations = PopulationSerializer(source='population_set', many=True, read_only=True)
+
+    class Meta:
+        model = Experiment
+        fields = (
+            "name", "is_private", "description", "date_created", "last_modified", "owner", "teams", "collaborators",
+            "populations")
