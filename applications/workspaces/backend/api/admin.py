f9d9b8ec2a5a03bc1ebd47a0d82bb0c2555536dd
afonso <afonsobspinto@gmail.com>
2022-03-14 12:02:22 +0000

    #32 Add population to admin

--- applications/workspaces/backend/api/admin.py
+++ applications/workspaces/backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from api.models import Experiment, Collaborator
+from api.models import Experiment, Collaborator, Population
+
 
 # Register your models here.
 
@@ -8,12 +9,19 @@ class ExperimentAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_private', 'last_modified')
     search_fields = ('name', 'owner__email')
     autocomplete_fields = ('owner',)
-    filter_horizontal = ('teams', )
+    filter_horizontal = ('teams',)
+
 
 class CollaboratorAdmin(admin.ModelAdmin):
     list_display = ('user', 'experiment', 'role')
-    autocomplete_fields = ('user', )
-    raw_id_fields = ('experiment', )
+    autocomplete_fields = ('user',)
+    raw_id_fields = ('experiment',)
+
+
+class PopulationAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(Experiment, ExperimentAdmin)
 admin.site.register(Collaborator, CollaboratorAdmin)
+admin.site.register(Population, PopulationAdmin)
