a436e70bafeb51264881b496c840758ddaa5e227
paularah <p.arah@alustudent.com>
2022-03-18 10:58:41 +0200

    merge branch develop into feature/41

--- applications/workspaces/backend/api/models.py
+++ applications/workspaces/backend/api/models.py
@@ -1,7 +1,67 @@
+from enum import Enum
+
 from django.db import models
 from django.contrib.auth.models import Group, User
 
+
 # Create your models here.
+
+class NotificationMethodsChoice(models.TextChoices):
+    @classmethod
+    def to_str(cls, value):
+        return next(v for v in list(AtlasesChoice) if v.value == value).label
+
+    EMAIL = "email", "Email"
+
+class UserDetail(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    notification_method = models.CharField(max_length=5, choices=NotificationMethodsChoice.choices, default=NotificationMethodsChoice.EMAIL)
+    notify_on_new_share = models.BooleanField(default=False)
+    notify_on_new_team_invite = models.BooleanField(default=False)
+    notify_on_clone_my_experiment = models.BooleanField(default=False)
+    notify_on_news = models.BooleanField(default=False)
+    avatar = models.ImageField(upload_to='avatars')
+
+    @staticmethod
+    def has_read_permission(request):
+        return True
+
+    @staticmethod
+    def has_write_permission(request):
+        return True
+
+    def has_object_read_permission(self, request):
+        return self.user == request.user
+
+    def has_object_write_permission(self, request):
+        return self.user == request.user
+
+class AtlasesChoice(models.TextChoices):
+    @classmethod
+    def to_str(cls, value):
+        return next(v for v in list(AtlasesChoice) if v.value == value).label
+
+    SLK10 = "slk10", "Salk cord 10um"
+    ALN20 = "aln20", "Allen cord 20um"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+
+    @staticmethod
+    def has_read_permission(request):
+        return True
+
+    @staticmethod
+    def has_list_permission(request):
+        return True
+
+    @staticmethod
+    def has_write_permission(request):
+        return True
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class CollaboratorRole(models.TextChoices):
@@ -20,8 +80,8 @@ class ExperimentsObjectsManager(models.Manager):
     def team_experiments(self, user):
         return (
             self.get_queryset()
-            .filter(teams__user=user)  # my teams experiments
-            .distinct()
+                .filter(teams__user=user)  # my teams experiments
+                .distinct()
         )
 
     def collaborate_experiments(self, user):
@@ -33,20 +93,23 @@ class ExperimentsObjectsManager(models.Manager):
         return self.get_queryset().filter(is_private=False)  # all public experiments
 
     def list(self, user):
-        return self.my_experiments(user) \
-            .union(self.team_experiments(user)) \
-            .union(self.collaborate_experiments(user)) \
-            .union(self.public_experiments())
+        return self.get_queryset().filter(id__in=self.list_ids(user))
 
+    def list_ids(self, user):
+        return self.my_experiments(user).only("id") \
+            .union(self.team_experiments(user).only("id")) \
+            .union(self.collaborate_experiments(user).only("id")) \
+            .union(self.public_experiments().only("id"))
 
 class Experiment(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     is_private = models.BooleanField(default=True)
-    date_created = models.DateField(auto_created=True)
-    last_modified = models.DateField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     teams = models.ManyToManyField(Group, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
     collaborators = models.ManyToManyField(
         User, related_name="collaborators", through="Collaborator"
     )
@@ -74,20 +137,20 @@ class Experiment(models.Model):
 
     def has_object_write_permission(self, request):
         return (
-            self.owner == request.user
-            or (
-                self.teams
-                and len(self.teams.filter(team__group__user=request.user)) > 0
-            )
-            or (
-                self.collaborators
-                and len(
+                self.owner == request.user
+                or (
+                        self.teams
+                        and len(self.teams.filter(team__group__user=request.user)) > 0
+                )
+                or (
+                        self.collaborators
+                        and len(
                     self.collaborator_set.filter(
                         user=request.user.id, role=CollaboratorRole.EDITOR
                     )
                 )
-                > 0
-            )
+                        > 0
+                )
         )
 
 
@@ -97,6 +160,46 @@ class Collaborator(models.Model):
     role = models.CharField(
         max_length=1, choices=CollaboratorRole.choices, default=CollaboratorRole.VIEWER
     )
+    shared_on = models.DateTimeField(auto_now_add=True)
+
+    @staticmethod
+    def has_read_permission(request):
+        return True
+
+    @staticmethod
+    def has_list_permission(request):
+        return True
+
+    @staticmethod
+    def has_write_permission(request):
+        experiment_id = request.data.get("experiment")
+        try:
+            experiment = Experiment.objects.get(id=experiment_id)
+            return experiment.has_object_write_permission(request)
+        except Experiment.DoesNotExist:
+            return True
+
+    def has_object_read_permission(self, request):
+        return self.experiment.has_object_read_permission(request) or self.user == request.user
+
+    def has_object_write_permission(self, request):
+        return self.experiment.has_object_write_permission(request) or self.user==request.user
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}, {self.experiment} ({CollaboratorRole.to_str(self.role)})"
+
+    class Meta:
+        unique_together = [['user', 'experiment']]
+
+class Population(models.Model):
+    experiment = models.ForeignKey(Experiment, on_delete=models.DO_NOTHING)
+    atlas = models.CharField(max_length=5, choices=AtlasesChoice.choices, default=AtlasesChoice.SLK10)
+    name = models.CharField(max_length=100, unique=True)
+    color = models.CharField(max_length=7)  # hex color
+
+
+class Cell(models.Model):
+    x = models.DecimalField(decimal_places=2, max_digits=6)
+    y = models.DecimalField(decimal_places=2, max_digits=6)
+    z = models.DecimalField(decimal_places=2, max_digits=6)
+    population = models.ForeignKey(Population, on_delete=models.CASCADE)
