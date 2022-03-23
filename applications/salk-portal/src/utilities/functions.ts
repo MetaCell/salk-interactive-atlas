5cc0775f86e2b6babcdfbcacc1e68fe02829c321
afonso <afonsobspinto@gmail.com>
2022-03-23 16:44:00 +0000

    #63 WIP - Retrieve real experiment

--- applications/salk-portal/src/utilities/functions.ts
+++ applications/salk-portal/src/utilities/functions.ts
@@ -1,19 +1,20 @@
 import {getAtlas} from "../service/AtlasService";
 import {AtlasChoice} from "./constants";
 import Range from "../models/Range";
+import {ExperimentCells} from "../apiclient/workspaces";
 
-export const areAllSelected = (obj: { [x: string]: { selected: any } }) => {
+export const areAllSelected = (obj: { [x: string]: { selected: any } }) : boolean => {
    return Object.keys(obj)
         .reduce((acc, sId) => obj[sId].selected && acc, true)
 }
 
-export function eqSet(set1: Set<any>, set2: Set<any>) {
+export function eqSet(set1: Set<any>, set2: Set<any>) : boolean {
    if (set1.size !== set2.size) return false;
    for (const x of set1) if (!set2.has(x)) return false;
    return true;
 }
 
-export const getAllowedRanges = (selectedAtlas: AtlasChoice, activeSubdivisions: Set<string>) => {
+export const getAllowedRanges = (selectedAtlas: AtlasChoice, activeSubdivisions: Set<string>) : Range[] => {
    const ranges : Range[] = []
    const atlas = getAtlas(selectedAtlas)
    activeSubdivisions.forEach(subdivisionId => {
@@ -21,4 +22,15 @@ export const getAllowedRanges = (selectedAtlas: AtlasChoice, activeSubdivisions:
       ranges.push(atlasSegment.range)
    });
    return ranges
+}
+
+export const getCells = async (uri: string) : Promise<ExperimentCells[]> => {
+   const response = await fetch(uri)
+   const reader = response.body.getReader();
+   const decoder = new TextDecoder('utf-8');
+   const result = await reader.read()
+   const csv = decoder.decode(result.value);
+   console.log(csv)
+   return []
+
 }
