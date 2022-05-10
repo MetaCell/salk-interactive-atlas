8a76c6161f037b6d9ecb1fd2e205af97980b09ac
afonso <afonsobspinto@gmail.com>
2022-05-10 15:26:21 +0100

    #108 chore: Update density map calculation

--- applications/workspaces/backend/api/helpers/density_map.py
+++ applications/workspaces/backend/api/helpers/density_map.py
@@ -1,7 +1,8 @@
-import numpy as np
-from cordmap.postprocessing.prob_map import generate_prob_map
-from PIL import Image
+import math
 
+import numpy as np
+from PIL import Image
+from skimage.filters import gaussian
 from api.helpers.atlas import get_bg_atlas
 from api.services.population_service import get_cells
 
@@ -9,39 +10,78 @@ ROSTRAL = "Rostral"
 CAUDAL = "Caudal"
 
 
-def _set_min_depth_cells(cells):
-    """
-    Given a list o @cells we change the depth of each cell to the min depth we can find in the array
-    Used as sort of a workaround to generate the probability map as we want though cordmap api
-    """
-    if len(cells) == 0:
-        return 0, []
-    cells_list = []
-    min_depth = int(min([cell[0] for cell in cells]))
-    for cell in cells:
-        cells_list.append([min_depth, cell[1], cell[2]])
-    return min_depth, np.array(cells_list)
-
-
 def generate_density_map(atlas, subdivision, populations):
     bg_atlas = get_bg_atlas(atlas)
-    min_depth, cells = _set_min_depth_cells(get_cells(subdivision, populations))
-    save_prob_map = False
-    output_directory = "N/A"
-    mask_prob_map = True
+    cells = get_cells(subdivision, populations)
     prob_map_smoothing = 100
     prob_map_normalise = True
+    bin_limits = get_subdivision_bin_limits(atlas, subdivision)
+
     probability_map = generate_prob_map(
-        output_directory,
         cells,
         bg_atlas,
-        save_prob_map,
-        mask=mask_prob_map,
+        bin_limits=bin_limits,
         smoothing=prob_map_smoothing,
         normalise=prob_map_normalise,
     )
-    img = probability_map[min_depth]
+    img = get_accumulated_probability_map(probability_map)
     scaled_image = 256 / np.max(img) * img
-    density_img = Image.fromarray(scaled_image)
-    density_img = density_img.convert("RGB")
-    return density_img
+
+    i = Image.fromarray(scaled_image)
+    i = i.convert('RGBA')
+    mask = i.convert('L')
+    i.putalpha(mask)
+
+    return i
+
+
+def get_bins(image_size, bin_sizes, bin_limits):
+    """
+    Given an image size, and bin size, return a list of the bin boundaries
+    :param image_size: Size of the final image (tuple/list)
+    :param bin_sizes: Bin sizes corresponding to the dimensions of
+    "image_size" (tuple/list)
+    :param bin_limits: Bin limits corresponding to the dimensions of
+    "image_size" (tuple/list)
+    :return: List of arrays of bin boundaries
+    """
+    bins = []
+    for dim in range(0, len(image_size)):
+        if bin_limits[dim]:
+            bins.append(np.arange(bin_limits[dim][0], bin_limits[dim][1] + 1, bin_sizes[dim]))
+        else:
+            bins.append(np.arange(0, image_size[dim] + 1, bin_sizes[dim]))
+    return bins
+
+
+def generate_prob_map(
+        points,
+        atlas,
+        bin_limits=(None, None, None),
+        normalise=True,
+        smoothing=None,
+):
+    bins = get_bins(atlas.annotation.shape, (1, 1, 1), bin_limits)
+    probability_map, _ = np.histogramdd(points, bins=bins, density=normalise)
+    if smoothing is not None:
+        smoothing = [int(round(smoothing / res)) for res in atlas.resolution]
+        probability_map = gaussian(probability_map, sigma=smoothing)
+    return probability_map
+
+
+def get_subdivision_bin_limits(atlas, subdivision):
+    segment, part = subdivision.split('-')
+    for s in atlas.metadata['atlas_segments']:
+        if s['Segment'] == segment:
+            if part == ROSTRAL:
+                return (s['Start'], int(math.floor((s['Start']+s['End'])/2))), None, None
+            else:
+                return (int(math.ceil((s['Start']+s['End']))/2), s['End']), None, None
+    return None, None, None
+
+
+def get_accumulated_probability_map(probability_map):
+    acc = np.zeros(probability_map[0].shape, dtype=float)
+    for slice in probability_map:
+        acc += slice
+    return acc / len(probability_map)
