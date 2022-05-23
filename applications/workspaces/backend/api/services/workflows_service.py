b8ab10dad77c61315c01dc14ce40628e9ddc99e4
Zoran Sinnema <zsinnema@gmail.com>
2022-05-23 12:01:20 +0200

    #111 fix: for issue with import of argo namespace on collectstatic

--- applications/workspaces/backend/api/services/workflows_service.py
+++ applications/workspaces/backend/api/services/workflows_service.py
@@ -1,12 +1,12 @@
 import uuid
 
-from cloudharness.workflows import tasks, operations
+import cloudharness.workflows as workflows
 
 GENERATE_IMAGES_IMAGE = "workspaces-generate_population_images"
 
 
 def _create_generate_population_images_task(population_id: int):
-    return tasks.CustomTask(
+    return workflows.tasks.CustomTask(
         name=f"{GENERATE_IMAGES_IMAGE}-{str(uuid.uuid4())[:8]}",
         image_name=GENERATE_IMAGES_IMAGE,
         population_id=population_id,
@@ -15,7 +15,7 @@ def _create_generate_population_images_task(population_id: int):
 
 def execute_generate_population_images_workflow(population_id: int):
     task = _create_generate_population_images_task(population_id)
-    op = operations.SingleTaskOperation(
+    op = workflows.operations.SingleTaskOperation(
         name=f"salk-generate-population-images-tasks-job",
         task=task
     )
