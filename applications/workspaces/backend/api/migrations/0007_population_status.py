70ebd56b8acc44bc5b966ecdec715e9f5f0821d9
afonso <afonsobspinto@gmail.com>
2022-05-23 15:15:00 +0100

    #111 chore: Add migrations

--- applications/workspaces/backend/api/migrations/0007_population_status.py
+++ applications/workspaces/backend/api/migrations/0007_population_status.py
@@ -6,14 +6,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0006_auto_20220519_0957'),
+        ("api", "0006_auto_20220519_0957"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='population',
-            name='status',
-            field=models.IntegerField(choices=[(-1, 'Error'), (0, 'Running'), (1, 'Ready')], default=1),
+            model_name="population",
+            name="status",
+            field=models.IntegerField(
+                choices=[(-1, "Error"), (0, "Running"), (1, "Ready")], default=1
+            ),
             preserve_default=False,
         ),
+
     ]
