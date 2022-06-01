1b9d4ca442163ed36d941f2e81fefb6be8a9ac4c
afonso <afonsobspinto@gmail.com>
2022-06-01 19:24:56 +0100

    #91 chore: Fix incorrect filepath

--- infrastructure/common-images/workspaces-base/backend/api/helpers/generate_population_cells.py
+++ infrastructure/common-images/workspaces-base/backend/api/helpers/generate_population_cells.py
@@ -15,4 +15,4 @@ def get_cells_filepath(data_filepath, storage_path):
     register_sections_3D(data_filepath, out_dir)
     filepath = move_file(os.path.join(out_dir, f"{CELLS_CSV}.csv"), storage_path,
                          f"{CELLS_CSV}_{str(uuid.uuid4())[:8]}.csv")
-    return get_persistence_path(filepath)
+    return filepath
