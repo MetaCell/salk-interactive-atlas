f6f486b9cb42fb70467a561c89f1004b50ef44dd
afonso <afonsobspinto@gmail.com>
2022-08-26 12:54:35 +0100

    #9 feat: Make grid dimensions atlas dependent

--- infrastructure/common-images/workspaces-base/backend/api/helpers/density_map/grid_creator.py
+++ infrastructure/common-images/workspaces-base/backend/api/helpers/density_map/grid_creator.py
@@ -8,20 +8,22 @@ from matplotlib import rcParams
 from api.helpers.ICustomAtlas import ICustomAtlas
 from api.helpers.atlas import get_subdivision_boundaries
 from api.helpers.image_manipulation import fig_to_img
+from api.utils import get_closest_multiple
 from workspaces.settings import (
     GRID_COLOR,
     GRID_CONSTANT_BOTTOM_OFFSET,
     GRID_CONSTANT_RIGHT_OFFSET,
     GRID_SUBREGION_DEPTH_ZERO_REFERENCE,
-    UM_TO_MM, GRID_FONT_SIZE, GRID_X_RANGE, GRID_Y_RANGE, GRID_DIMENSIONS_MM, GRID_Z_TITLE_PAD, GRID_X_LABEL_ROTATION,
-    GRID_Z_TITLE_VERTICAL_AXES_LOCATION, GRID_AXIS_LABEL_PAD, GRID_X_LABEL_LOC, GRID_Y_LABEL_LOC,
+    UM_TO_MM, GRID_FONT_SIZE, GRID_X_RANGE, GRID_Y_RANGE, GRID_Z_TITLE_PAD, GRID_X_LABEL_ROTATION,
+    GRID_Z_TITLE_VERTICAL_AXES_LOCATION, GRID_AXIS_LABEL_PAD, GRID_X_LABEL_LOC, GRID_Y_LABEL_LOC, GRID_Z_TITLE_LOC,
+    GRID_Z_TITLE_VERTICAL_ALIGNMENT, GRID_Z_TITLE_HORIZONTAL_ALIGNMENT, GRID_UNIT, GRID_Z_TITLE, GRID_TICK_STEP,
 )
 
 
 def get_grid_image(
         bg_atlas: ICustomAtlas, subdivision: str
 ) -> Image:
-    w, h = GRID_DIMENSIONS_MM
+    w, h = get_grid_dimensions(bg_atlas)
 
     fig, ax = plt.subplots()
     # Updates figure size to coop with the labels and ticks
@@ -30,6 +32,7 @@ def get_grid_image(
     x_shape, y_shape = _set_size(w, h, ax)
 
     resolution_z = 1 / (bg_atlas.resolution[0] * UM_TO_MM)
+
 
     # calculates ratio from image size to atlas size
     ratio_x = w / x_shape
@@ -42,19 +45,19 @@ def get_grid_image(
     ax.set_yticks(y_ticks)
     ax.set_xticklabels(x_ticks_labels, rotation=GRID_X_LABEL_ROTATION)
     ax.set_yticklabels(y_ticks_labels)
-    ax.set_xlabel('mm', labelpad=GRID_AXIS_LABEL_PAD, loc=GRID_X_LABEL_LOC, fontsize=GRID_FONT_SIZE)
-    ax.set_ylabel('mm', labelpad=GRID_AXIS_LABEL_PAD, loc=GRID_Y_LABEL_LOC, fontsize=GRID_FONT_SIZE)
 
+    ax.set_xlabel(GRID_UNIT, labelpad=GRID_AXIS_LABEL_PAD, loc=GRID_X_LABEL_LOC, fontsize=GRID_FONT_SIZE)
+    ax.set_ylabel(GRID_UNIT, labelpad=GRID_AXIS_LABEL_PAD, loc=GRID_Y_LABEL_LOC, fontsize=GRID_FONT_SIZE)
     ax.set_title(
-        f"Z-axis position (center in C8 Caudal): {_get_subdivision_depth(bg_atlas, subdivision, resolution_z)} mm",
+        f"{GRID_Z_TITLE} {_get_subdivision_depth(bg_atlas, subdivision, resolution_z)} {GRID_UNIT}",
         fontdict={
             "fontsize": GRID_FONT_SIZE,
             "fontweight": rcParams["axes.titleweight"],
             "color": GRID_COLOR,
-            "verticalalignment": "baseline",
-            "horizontalalignment": "right",
+            "verticalalignment": GRID_Z_TITLE_VERTICAL_ALIGNMENT,
+            "horizontalalignment": GRID_Z_TITLE_HORIZONTAL_ALIGNMENT,
         },
-        loc="right",
+        loc=GRID_Z_TITLE_LOC,
         y=GRID_Z_TITLE_VERTICAL_AXES_LOCATION,
         pad=GRID_Z_TITLE_PAD
     )
@@ -84,6 +87,46 @@ def get_grid_image(
         img, 0, GRID_CONSTANT_RIGHT_OFFSET, GRID_CONSTANT_BOTTOM_OFFSET, 0
     )
     return shifted_image
+
+
+def get_grid_dimensions(bg_atlas: ICustomAtlas, tick_step: float = GRID_TICK_STEP, tolerance: float = 0.1) -> \
+        tuple[float, float]:
+    """
+    Gets the grid dimensions based on the {bg_atlas} shape 
+    Considering the {tick_step} and {tolerance} we make it so that the ticks happen on 'nice' places and not congested
+    """
+
+    resolution_height = 1 / (bg_atlas.resolution[1] * UM_TO_MM)
+    resolution_width = 1 / (bg_atlas.resolution[2] * UM_TO_MM)
+    _, atlas_height, atlas_width = bg_atlas.shape
+
+    return _get_best_grid_dimension(atlas_width * resolution_width, tick_step, tolerance), \
+           _get_best_grid_dimension(atlas_height * resolution_height, tick_step, tolerance)
+
+
+def _get_best_grid_dimension(axis_dimension: float, step: float, tolerance: float = 0.1) -> float:
+    """
+    Chooses a multiple of {step} with a difference of at least {tolerance}% to the {dimension} value
+    so that we get a 'pleasant to the eyes' - aka with some white space - grid dimension
+    """
+    semi_axis_dimension = axis_dimension / 2
+    if tolerance > 1 or tolerance < 0:
+        return axis_dimension
+    multiplier, semi_axis_best_dimension = get_closest_multiple(semi_axis_dimension, step)
+    if _exceeds_tolerance(semi_axis_best_dimension, semi_axis_dimension, tolerance):
+        return semi_axis_best_dimension * 2
+    iterations = 1 / tolerance
+    while iterations > 0:
+        multiplier += 1
+        semi_axis_best_dimension = step * multiplier
+        if _exceeds_tolerance(semi_axis_best_dimension, semi_axis_dimension, tolerance):
+            return semi_axis_best_dimension * 2
+        iterations -= 1
+    return axis_dimension
+
+
+def _exceeds_tolerance(new_dimension: float, dimension: float, tolerance: float) -> float:
+    return (new_dimension - dimension) / dimension > tolerance
 
 
 def _get_ticks(rg: tuple, img_to_res_ratio: float, step: float = 0.25):
