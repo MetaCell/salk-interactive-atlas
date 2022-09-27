#208 fix: Fix pad calculation from offset

applications/portal/backend/api/helpers/image_manipulation.py
@@ -1,4 +1,5 @@
 import io
+import math
 
 import numpy
 import numpy as np
@@ -58,8 +59,8 @@ def fig_to_numpy(fig: Figure) -> np.array:
 
 def pad_image(pil_img, top, right, bottom, left, color=(255, 0, 0, 0)) -> Image:
     width, height = pil_img.size
-    new_width = int(width + right + left)
-    new_height = int(height + top + bottom)
+    new_width = math.ceil(width + right + left)
+    new_height = math.ceil(height + top + bottom)
     result = Image.new(pil_img.mode, (new_width, new_height), color)
     result.paste(pil_img, (left, top))
     return result