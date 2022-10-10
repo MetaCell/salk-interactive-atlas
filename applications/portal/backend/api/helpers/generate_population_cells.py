11105a09f8e8662cac15273a7fec7f550b28647d
Zoran Sinnema <zsinnema@gmail.com>
2022-10-10 21:35:38 +0200

    #212 chore: disable parallel generation of 3D sections

--- applications/portal/backend/api/helpers/generate_population_cells.py
+++ applications/portal/backend/api/helpers/generate_population_cells.py
@@ -11,7 +11,7 @@ CELLS_CSV = "cells_with_labels"
 def get_cells_filepath(population_name: str, data_filepath: str, storage_path: str):
     from cordmap.main import register_sections_3D
     out_dir = os.path.join(os.path.dirname(data_filepath), OUTPUT_DIRECTORY)
-    register_sections_3D(data_filepath, out_dir)
+    register_sections_3D(data_filepath, out_dir, parallel=False)
     filepath = move_file(
         os.path.join(out_dir, f"{CELLS_CSV}_{population_name}.csv"),
         storage_path,
