5bf1976825995f6eaa6af5994ca3e5a6750a369a
Zoran Sinnema <zsinnema@gmail.com>
2022-10-12 11:01:38 +0200

    chore: pinned django postgres pool to v2.0.5

--- applications/portal/backend/workspaces/settings.py
+++ applications/portal/backend/workspaces/settings.py
@@ -225,34 +225,8 @@ UM_TO_MM = 10
 
 POSITION_WITHIN_SUBSEGMENT = 0.5  # Position (in percentage) of the slice to pick from a subsegment
 
-
-import logging
-# Get an instance of a logger
-logger = logging.getLogger(__name__)
-logger.info("*"*80)
-logger.info(" ")
-logger.info(DATABASES)
-logger.info(" ")
-
+# for postgres configure to use the database pool
 if DATABASES.get("default", None) and DATABASES.get("default").get("ENGINE", None) == "django.db.backends.postgresql":
     DATABASES.get("default").update({
-        "DISABLE_SERVER_SIDE_CURSORS": True,
-        "CONN_MAX_AGE": 0,
         "ENGINE": "django_postgrespool2"
     })
-
-logger.info(DATABASES)
-logger.info(" ")
-logger.info("*"*80)
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "cloudharness",
-#         "USER": "mnp",
-#         "PASSWORD": "metacell",
-#         "HOST": "portal-db",
-#         "PORT": 5432,
-#         "CONN_MAX_AGE": 0,
-#         "DISABLE_SERVER_SIDE_CURSORS": True
-#     },
-# }
