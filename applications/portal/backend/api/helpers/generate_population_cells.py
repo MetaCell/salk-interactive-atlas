fdd2afc2e38671ee1a3f7919d2931f8dd40715d9
afonso <afonsobspinto@gmail.com>
2023-03-16 15:08:20 +0000

    #244 fix: Fix get cells filepath from single file

--- applications/portal/backend/api/helpers/generate_population_cells.py
+++ applications/portal/backend/api/helpers/generate_population_cells.py
@@ -4,7 +4,8 @@ import uuid
 from api.services.filesystem_service import move_file
 
 OUTPUT_DIRECTORY = "./out"
-CELLS_CSV = "cells_with_labels"
+PAIR_CELLS_CSV_SUFFIXE = "cells_with_labels"
+SINGLE_CELLS_CSV_SUFFIXE = "transformed_cells"
 
 
 def get_cells_filepath(population_name: str, data_filepath: str, storage_path: str, is_fiducial: bool = False):
@@ -19,9 +20,9 @@ def _get_cells_filepath_from_file_pair(population_name: str, data_filepath: str,
     out_dir = os.path.join(os.path.dirname(data_filepath), OUTPUT_DIRECTORY)
     register_sections_3D(data_filepath, out_dir, parallel=False)
     filepath = move_file(
-        os.path.join(out_dir, f"{CELLS_CSV}_{population_name}.csv"),
+        os.path.join(out_dir, f"{PAIR_CELLS_CSV_SUFFIXE}_{population_name}.csv"),
         storage_path,
-        f"{CELLS_CSV}_{str(uuid.uuid4())[:8]}.csv",
+        f"{PAIR_CELLS_CSV_SUFFIXE}_{str(uuid.uuid4())[:8]}.csv",
     )
     return filepath
 
@@ -30,9 +31,9 @@ def _get_cells_filepath_from_single_file(population_name: str, filepath: str, st
     from cordmap.entry_points.register_3D_fiducial import register_3D_fiducial
     out_dir = os.path.join(os.path.dirname(filepath), OUTPUT_DIRECTORY)
     register_3D_fiducial(filepath, out_dir)
-    filepath = move_file(
-        os.path.join(out_dir, f"{CELLS_CSV}_{population_name}.csv"),
+    fpath = move_file(
+        os.path.join(out_dir, f"{SINGLE_CELLS_CSV_SUFFIXE}_{population_name}.csv"),
         storage_path,
-        f"{CELLS_CSV}_{str(uuid.uuid4())[:8]}.csv",
+        f"{SINGLE_CELLS_CSV_SUFFIXE}_{str(uuid.uuid4())[:8]}.csv",
     )
-    return filepath
+    return fpath
