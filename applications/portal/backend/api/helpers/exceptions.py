ec31512a35d0d4b6394c05b95067d7c8887b9c27
afonso <afonsobspinto@gmail.com>
2023-04-28 15:49:40 +0100

    #255 feat: Allow adding populations to existent experiment

--- applications/portal/backend/api/helpers/exceptions.py
+++ applications/portal/backend/api/helpers/exceptions.py
@@ -6,8 +6,19 @@ class AuthorizationError(Exception):
 
 class InvalidInputError(Exception):
     """Exception raised for invalid input errors"""
-
     pass
+
+
+class InvalidPopulationFile(Exception):
+    """Exception raised for invalid input errors"""
+    def __init__(self, repeated_population_name):
+        super().__init__(f"File contains repeated population: {repeated_population_name}")
+
+
+class DuplicatedPopulationError(Exception):
+    """Exception raised for duplicate populations"""
+    def __init__(self, duplicated_population):
+        super().__init__(f"Duplicated population: {duplicated_population}")
 
 
 class DensityMapIncorrectSubdivisionError(Exception):
