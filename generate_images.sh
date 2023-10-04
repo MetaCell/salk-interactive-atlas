917adbd81658bd0964ab63d629043ccd123352e8
D. Gopal Krishna <nikhil0223@gmail.com>
2023-10-04 17:08:20 +0530

    docs update image generate script

--- generate_images.sh
+++ generate_images.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd applications/portal/backend/
+# set the values for the variables below
+cd applications/portal/frontend
+BUILDDIR=$(pwd)
+cd ../backend
+APP_DIR=$(pwd)
 
 # Run Django management commands
 python3 manage.py generate_canal_images salk_cord_10um
@@ -9,4 +13,4 @@ python3 manage.py generate_lamina_images salk_cord_10um
 python3 manage.py generate_annotation_images salk_cord_10um
 
 # Copy files from the backend stage
-cp -r ${APP_DIR}/persistent/ ${BUILDDIR}/src/assets/atlas/salk_cord_10um/
+cp -r ${APP_DIR}/persistent/* ${BUILDDIR}/src/assets/atlas/salk_cord_10um/
