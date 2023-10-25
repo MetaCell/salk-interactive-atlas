d13fe22d40a2d6e312f85c42b67ca0f64490baa8
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2023-10-25 14:35:23 +0200

    #273 text editor changed as well

--- applications/portal/frontend/src/components/common/ExperimentDialogs/TextEditor.jsx
+++ applications/portal/frontend/src/components/common/ExperimentDialogs/TextEditor.jsx
@@ -5,19 +5,24 @@ import ITALIC from "../../../assets/images/icons/italic.svg";
 import UNDERLINE from "../../../assets/images/icons/underline.svg";
 import UNORDERED from "../../../assets/images/icons/unordered_list.svg";
 import ORDERED from "../../../assets/images/icons/ordered_list.svg";
-import {Editor} from "react-draft-wysiwyg";
+import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import {EditorState} from 'draft-js';
+import { EditorState, ContentState, convertFromHTML } from 'draft-js';
 
 export const TextEditor = (props) => {
-    const [editorState, setEditorState] = React.useState(EditorState.createEmpty())
-    const {onChange, wrapperClassName} = props;
-
+    const { value, onChange, wrapperClassName } = props;
+    const [editorState, setEditorState] = React.useState(EditorState.createEmpty());
 
     const onEditorStateChange = (updatedEditorState) => {
         setEditorState(updatedEditorState)
         onChange(updatedEditorState)
-    }
+    };
+
+    React.useEffect(() => {
+        if (value) {
+            setEditorState(EditorState.createWithContent(ContentState.createFromBlockArray(convertFromHTML(value))));
+        };
+    }, []);
 
     return (
         <Editor
@@ -28,22 +33,22 @@ export const TextEditor = (props) => {
                 options: ['inline', 'list', 'link'],
                 inline: {
                     options: ['bold', 'italic', 'underline'],
-                    bold: {icon: BOLD},
-                    italic: {icon: ITALIC},
-                    underline: {icon: UNDERLINE},
+                    bold: { icon: BOLD },
+                    italic: { icon: ITALIC },
+                    underline: { icon: UNDERLINE },
                 },
                 list: {
                     inDropdown: false,
                     options: ['unordered', 'ordered'],
-                    unordered: {icon: UNORDERED},
-                    ordered: {icon: ORDERED},
+                    unordered: { icon: UNORDERED },
+                    ordered: { icon: ORDERED },
                 },
                 link: {
                     inDropdown: false,
                     showOpenOptionOnHover: true,
                     defaultTargetOption: '_blank',
                     options: ['link'],
-                    link: {icon: LINK},
+                    link: { icon: LINK },
                 },
             }}
         />
